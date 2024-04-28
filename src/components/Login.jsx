63c21b19bd9cc1c0dd3c57c263a1c8c8f6682f98
Kshiyo <theycallmeanomaly@gmail.com>
2024-04-29 01:10:52 +0530

    OK

---
+++ src/components/Login.jsx
@@ -0,0 +1,178 @@
+import React from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+import {Link , useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState , useEffect } from 'react'
+
+function Login() {
+
+  const navigate = useNavigate()
+  const noInput = () => toast.error('Please enter password'); 
+  
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [show , setShow] = useState(false)
+
+  const valueInput = document.getElementById("pass")?.value
+
+  const handleClick = (e) => {
+    if(valueInput){
+      setShow(!show)
+    }else{
+      noInput()
+    }
+  }
+
+  useEffect(() => {
+    if(show){
+      document.getElementById("pass").type = "text"
+    }else {
+      document.getElementById("pass").type = "password"
+    }
+  }, [show])
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value) 
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleLogin = async(e) => {
+    e.preventDefault()
+
+    
+
+    console.log(username , password , email)
+
+    // const formData = new FormData()
+    // formData.append('username' , username)
+    // formData.append('password' , password)
+    // formData.append('email' , email)
+    
+
+    try {
+      const params = new URLSearchParams();
+      params.append('username', username);
+      params.append('password', password);
+      params.append('email', email);
+
+      
+      const response = await toast.promise(axios.post('http://localhost:5000/api/v1/users/login' , params ,
+      { headers: 
+        {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    ),
+    {
+      loading : "Logging In...",
+      error: "User does not exist"
+    }
+  );
+    //   const response = await axios.post('http://localhost:5000/api/v1/users/login' , formData ,
+    //   { headers: 
+    //     {
+    //       'Content-Type': 'multipart/form-data'
+    //     }
+    //   }
+    // );
+
+      // console.log(response.data.statusCode)
+      setUsername('')
+      setEmail('')
+      setPassword('')  
+
+      if(response.data.statusCode == 200){
+        navigate('/message')
+        }
+    
+
+
+    } catch (error) {
+        console.error('Error logging in: ', error)
+        setUsername('')
+        setEmail('')
+        setPassword('')
+    }
+    
+  }
+
+  return (
+  <>
+    <div className="flex w-screen h-screen">
+    <div className=" w-screen flex items-center justify-center"> 
+    <Toaster 
+        position='top-center
+        '/>
+    <div className='h-[1600px] w-screen bg-[url("../../public/Images/loginANDregBG4.jpeg")] bg-cover absolute'></div>
+    <div className='backdrop-blur w-[35%] h-[68%] relative bg-none top-[20px] px-20 py-5 rounded-3xl border-4 border-grey'>
+        
+        <div className='text-3xl mt-2 px-32 text-center text-white relative'>
+         <h1>Login</h1>
+        </div>
+        <p className='font-medium text-lg text-black-500 mt-10 text-center'></p>
+        <form onSubmit={handleLogin}>
+        <div>
+            <div className='mt-4 mb-4 relative'>
+                <input type="text"
+                  onChange = {handleUsername}
+                  value={username}
+                  className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                  placeholder='Username' 
+                  id='username'
+                  autocomplete='off'
+                />
+            </div>
+            <div className='mt-4 mb-4 relative'>
+                <input type="text"
+                  onChange = {handleEmail}
+                  value={email}
+                  className='w-full border-4  placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey text-white p-4  mt-1 bg-transparent relative'
+                  placeholder='Email' 
+                  id='email'
+                  autocomplete='off'
+                />
+            </div>
+            <div className='mt-4 mb-4 relative'>
+                <input type="password"
+                  onChange = {handlePassword}
+                  value={password}
+                  className='w-full border-4  placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey text-white p-4  mt-1 bg-transparent relative'
+                  placeholder='Password'
+                  id='pass' 
+
+                />
+                <div className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.02] ease-in-out hover:text-black absolute top-[23px] text-white opacity-80 right-4 cursor-pointer' onClick={handleClick}>
+                  {show ? "Hide": "Show"}
+                  </div>
+            </div>
+            <div className='mt-10 flex flex-col w-full relative'>
+                <button 
+                type='submit'
+                className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.03] ease-in-out py-3 rounded-full bg-white text-black-500 text-lg relative'>
+                    Login
+                </button>
+                <div className='mt-10 flex justify-center items-center relative'>
+                   <p className='font-small text-base text-white relative'>Don't have an account ?</p>
+                   <Link to = "/register">
+                   <button className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.08] ease-in-out hover:text-black py-3 text-white text-base font-medium ml-2 relative'>Register</button>
+                   </Link>
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
+    </div>
+    </div> 
+  </>  
+  )
+}
+
+export default Login
---
+++ src/components/Register.jsx
@@ -0,0 +1,293 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState , useEffect} from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+import axios from 'axios'
+
+
+function Register() {
+
+  // const notifyRegister = () => toast('Registration Successful !!'); 
+  const noInput = () => toast('Does not contain any password !!'); 
+
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [dob, setDob] = useState("")
+  const [file, setFile] = useState(null)
+  const [isClicked , setClicked] = useState(false)
+  const [show , setShow] = useState(false)
+  
+  const valueInput = document.getElementById("pass")?.value
+
+  const handleClick = (e) => {
+    if(valueInput){
+      setShow(!show)
+    }else{
+      noInput()
+    }
+    
+  }
+
+  
+  useEffect(() => {
+      if(show){
+        document.getElementById("pass").type = "text"
+      }else {
+        document.getElementById("pass").type = "password"
+      }
+  }, [show])
+  
+  
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleDob = (e) => {
+    setDob(e.target.value)
+  }
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0])
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setClicked(true)
+
+    if(!file){
+      console.log("Please select a file")
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('avatar' , file)
+    formData.append('username' , username)
+    formData.append('password' , password)
+    formData.append('email' , email)
+    formData.append('dob' , dob)
+
+    // console.log(process.env.REGISTER_DOMAIN)
+    // you're trying to use the dotenv package in a browser environment, 
+    // which is not its intended use case. dotenv is primarily used in Node.js applications
+    // to load environment variables from a .env file into the process.env object.
+
+    try {
+      const response = await toast.promise(axios.post('http://localhost:5000/api/v1/users/register' , formData , {
+        headers: {
+          'Content-Type' : 'multipart/form-data'
+        }
+      }
+    ),
+    {
+      loading: "Registering...",
+      success: "Registration Successful",
+      error: "Registration failed."
+    }
+  );
+
+    //   console.log(response.data.statusCode) == 200
+    if(response.data.statusCode == 200){
+        setClicked(false)
+        // notifyRegister()
+    }
+
+      setUsername('')
+      setDob('')
+      setEmail('')
+      setPassword('')
+      setFile(null)
+      document.getElementById('avatar').value = '';
+    } catch (error) {
+        console.error('Error uploading file: ', error)
+        setUsername('')
+        setDob('')
+        setEmail('')
+        setPassword('')
+        setFile(null)
+        document.getElementById('avatar').value = '';
+    }
+
+    // try {
+    //   console.log(formData)
+    //   const response = await axios.post('http://localhost:5000/api/v1/users/register', formData);
+      
+    //   // Handle response if needed
+    //   console.log('Server response:', response.data);
+    // } catch (error) {
+    //   console.error('Error submitting form:', error);
+    // }
+
+  }
+
+  return (
+  <>
+    <div className="flex w-screen h-screen">
+    <div className=" w-screen flex items-center justify-center"> 
+    <Toaster
+          position='top-center'
+          /> 
+    <div className='h-[1600px] w-screen bg-[url("../../public/Images/loginANDregBG4.jpeg")] bg-cover absolute'></div>
+        <div className='backdrop-blur top-[20px] bg-none px-10 py-5 rounded-3xl border-4 border-grey relative h-max'>
+        
+            <h1 className='text-3xl mt-2 text-center text-white relative px-32'>Register</h1>
+            <p className='font-medium text-lg text-gray-500 mt-6'></p>
+            <form onSubmit={handleSubmit} >
+            <div>
+                <div className='mt-3 mb-3 relative'>
+                    {!isClicked ? (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                      placeholder='Enter your username' 
+                      id='username'
+                      autocomplete='off'
+                      value={username}
+                      onChange={handleUsername}
+                      />
+                    ) : (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your username' 
+                    id='username'
+                    autocomplete='off'
+                    value={username}
+                    onChange={handleUsername}
+                    readOnly
+                    />
+                    ) }
+                    
+                </div>
+                <div className='mt-3 mb-3 relative'>
+                    {!isClicked ? (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                      placeholder='Enter your email' 
+                      id='email'
+                      autocomplete='off'
+                      value={email}
+                      onChange={handleEmail}
+                      />
+                    ) : (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your email' 
+                    id='email'
+                    autocomplete='off'
+                    value={email}
+                    onChange={handleEmail}
+                    readOnly
+                    />
+                    ) }
+                    
+                </div>
+                <div className='mt-3 mb-3 relative'>
+                    {!isClicked ? (
+                    <input
+                    type='password'
+                    className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your password'
+                    id='pass' 
+                    value={password}
+                    onChange={handlePassword}
+                    />
+                    ) : (
+                      <input type="password"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your password'
+                    id='pass' 
+                    value={password}
+                    onChange={handlePassword}
+                    readOnly
+                    />
+                    ) }
+                  <div className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.02] ease-in-out hover:text-black absolute top-[23px] text-white opacity-80 right-4 cursor-pointer' onClick={handleClick}>
+                  {show ? "Hide": "Show"}
+                  </div>
+                </div>
+                <div className='mt-3 mb-3 relative'>
+                    {!isClicked ? (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your birth date' 
+                    id='dob'
+                    autocomplete='off'
+                    value={dob}
+                    onChange={handleDob}
+                    />
+                    ) : (
+                      <input type="text"
+                      className='w-full border-4 placeholder:text-white placeholder:opacity-80 focus:outline-none focus:border-white rounded-full border-grey p-4 text-white mt-1 bg-transparent relative'
+                    placeholder='Enter your birth date' 
+                    id='dob'
+                    autocomplete='off'
+                    value={dob}
+                    onChange={handleDob}
+                    readOnly
+                    />
+                    ) }
+                    
+                </div>
+                <div className='mt-4 mb-4'>
+                      <label className='text-small text-white' htmlFor="avatar">Choose your profile: </label>
+                      {!isClicked ? (
+                        <input id="avatar" type="file" onChange={handleFileChange} className="block w-full text-sm text-slate-500
+                        file:mr-4 file:py-2 file:px-4
+                        file:rounded-full file:border-0
+                        file:text-sm file:font-semibold
+                        file:bg-violet-50 file:text-button
+                        hover:file:bg-violet-100
+                        "/>
+                      ) : 
+                      (
+                        <input id="avatar" type="file" className="block w-full text-sm text-slate-500
+                        file:mr-4 file:py-2 file:px-4
+                        file:rounded-full file:border-0
+                        file:text-sm file:font-semibold
+                        file:bg-violet-50 file:text-button
+                        hover:file:bg-violet-100"
+                        readOnly
+                        />
+                      )
+                      }     
+                </div>
+                <div className='mt-8 flex flex-col w-full'>
+                    {!isClicked ? (
+                        <button
+                        type='submit'
+                        className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.03] ease-in-out py-3 rounded-full bg-white text-black text-lg relative'>
+                            Register
+                        </button>
+                    ) : (
+                        <button
+                        disabled
+                        className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.03] ease-in-out py-3 rounded-full bg-white text-black-500 text-lg relative'>
+                            Register
+                        </button>
+                    )}
+                    <div className='mt-8 flex justify-center items-center'>
+                    <p className='font-medium text-white text-base'>Already have an account ?</p>
+                    <Link to="/login">
+                    <button className='active:scale-[.98] active:duration-75  transition-all hover:scale-[1.08] ease-in-out hover:text-black py-3 text-white text-base font-medium ml-2 relative'>Login</button>
+                    </Link>
+                    </div>
+                </div>
+            </div>
+            </form>
+        </div>   
+        </div>
+        </div>
+  </>  
+  )
+}
+
+export default Register
